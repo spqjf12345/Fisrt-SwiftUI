be827ae42f26e5f007f47e5591b1dc9ab3435c0a
jouureee <jouureee@gmail.com>
2022-03-29 21:21:46 +0900

    100days of swiftUI ~day 18

---
+++ firstSwiftUI/WeSplitApp/WeSplitApp.swift
@@ -0,0 +1,20 @@
+//
+//  WeSplitApp.swift
+//  firstSwiftUI
+//
+//  Created by JoSoJeong on 2022/03/29.
+//
+
+import SwiftUI
+
+struct WeSplitApp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeSplitApp_Previews: PreviewProvider {
+    static var previews: some View {
+        WeSplitApp()
+    }
+}
